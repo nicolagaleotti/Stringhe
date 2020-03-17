a6b51a9707063b7b2aa3446a62d0d43c98464657
Nicola Galeotti <niccio2802@outlook.it>
2020-03-17 16:24:40 +0100

    Add project files.

---
+++ ConsoleProva/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EserciziStringhe;
+
+namespace ConsoleProva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prova AreEquals
+            Console.Write("stringa 1: ");
+            string s1 = Console.ReadLine();
+            Console.Write("stringa 2: ");
+            string s2 = Console.ReadLine();
+            bool risposta = Metodi.AreEquals(s1, s2);
+            Console.WriteLine(risposta);
+            //prova ContainsDoubleChar
+            Console.Write("stringa: ");
+            string stringa = Console.ReadLine();
+            bool risposta2 = Metodi.ContainsDoubleChar(stringa);
+            Console.WriteLine(risposta2);
+            //prova Convert
+        }
+    }
+}
---
+++ EserciziStringhe/Metodi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime;
+
+namespace EserciziStringhe
+{
+    public class Metodi
+    {
+        public static bool AreEquals (string s1, string s2) //pag. 262 numero 1
+        {
+            if (s1 == s2)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool ContainsDoubleChar(string s) //pag. 262 numero 2
+        {
+            int maiuscole = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsUpper(s[i]) == true)
+                    maiuscole++;
+            }
+            if (maiuscole > 0)
+                return true;
+            else 
+                return false;
+        }
+
+        public static string Convert (string s) //pag. 262 numero 11
+        {
+            return "";
+        }
+    }
+}
